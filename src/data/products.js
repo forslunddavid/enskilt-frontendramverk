46e54d2687e657f1efab17d6703ae5d545602657
Forslunddavid <forslund.david@gmail.com>
2023-05-05 00:26:44 +0200

    added product data

---
+++ src/data/products.js
@@ -0,0 +1,135 @@
+export const data = [
+	{
+		name: "Water slide",
+		price: 49.99,
+		description: "Giant inflatable water slide for hours of summer fun.",
+		picture: "",
+	},
+	{
+		name: "Beach ball",
+		price: 9.99,
+		description: "Classic beach ball for all your summer activities.",
+		picture: "",
+	},
+	{
+		name: "Frisbee",
+		price: 7.99,
+		description:
+			"Lightweight and durable frisbee for throwing on the beach or in the park.",
+		picture: "",
+	},
+	{
+		name: "Inflatable pool",
+		price: 39.99,
+		description:
+			"Easy to set up and store inflatable pool for summer relaxation.",
+		picture: "",
+	},
+	{
+		name: "Water balloons",
+		price: 5.99,
+		description:
+			"Pack of 100 colorful water balloons for epic water fights.",
+		picture: "",
+	},
+	{
+		name: "Sandcastle kit",
+		price: 12.99,
+		description:
+			"Complete kit with everything you need to build amazing sandcastles.",
+		picture: "",
+	},
+	{
+		name: "Kite",
+		price: 19.99,
+		description: "Easy-to-fly kite for beach or park adventures.",
+		picture: "",
+	},
+	{
+		name: "Bubble wand",
+		price: 4.99,
+		description: "Create endless bubbles with this fun bubble wand.",
+		picture: "",
+	},
+	{
+		name: "Water gun",
+		price: 14.99,
+		description: "Powerful water gun for epic water battles with friends.",
+		picture: "",
+	},
+	{
+		name: "Surfboard",
+		price: 299.99,
+		description: "High-quality surfboard for riding the waves this summer.",
+		picture: "",
+	},
+	{
+		name: "Paddleball set",
+		price: 29.99,
+		description: "Two-player paddleball set for beach or park fun.",
+		picture: "",
+	},
+	{
+		name: "Roller skates",
+		price: 79.99,
+		description:
+			"Classic roller skates for cruising around town or the boardwalk.",
+		picture: "",
+	},
+	{
+		name: "Pool Noodles",
+		price: 6.99,
+		description:
+			"Set of colorful pool noodles for endless summer fun in the water.",
+		picture: "",
+	},
+	{
+		name: "Giant Jenga",
+		price: 39.99,
+		description:
+			"Giant-sized Jenga game for outdoor summer fun with friends and family.",
+		picture: "",
+	},
+	{
+		name: "Inflatable flamingo",
+		price: 29.99,
+		description:
+			"Large and colorful flamingo pool float for relaxing summer days.",
+		picture: "",
+	},
+	{
+		name: "Squirt gun",
+		price: 9.99,
+		description:
+			"Fun and easy to use squirt gun for water fights and outdoor play.",
+		picture: "",
+	},
+	{
+		name: "Beach volleyball",
+		price: 19.99,
+		description:
+			"Official size and weight volleyball for beach and outdoor play.",
+		picture: "",
+	},
+	{
+		name: "Kite",
+		price: 14.99,
+		description:
+			"Colorful kite for fun and relaxing days at the park or beach.",
+		picture: "",
+	},
+	{
+		name: "Beach umbrella",
+		price: 39.99,
+		description:
+			"Large and sturdy beach umbrella for sun protection and relaxation.",
+		picture: "",
+	},
+	{
+		name: "Sandcastle building kit",
+		price: 12.99,
+		description:
+			"Complete sandcastle building kit with molds and tools for endless fun at the beach.",
+		picture: "",
+	},
+]

41afc2186bb6d8a9e7478ea718da32546b97fd69
Forslunddavid <forslund.david@gmail.com>
2023-05-06 19:44:21 +0200

    added pictures to product array

--- src/data/products.js
+++ src/data/products.js
@@ -5,14 +5,16 @@ export const data = [
 		name: "Water slide",
 		price: 49.99,
 		description: "Giant inflatable water slide for hours of summer fun.",
-		picture: "",
+		picture:
+			"https://www.thegreenhead.com/imgs/xl/aquaglide-summit-express-inflatable-water-slide-xl.jpg",
 		shopid: shopId,
 	},
 	{
 		name: "Beach ball",
 		price: 9.99,
 		description: "Classic beach ball for all your summer activities.",
-		picture: "",
+		picture:
+			"http://womantribune.com/wp-content/uploads/2015/08/beach-balls.jpg",
 		shopid: shopId,
 	},
 	{
@@ -20,7 +22,8 @@ export const data = [
 		price: 7.99,
 		description:
 			"Lightweight and durable frisbee for throwing on the beach or in the park.",
-		picture: "",
+		picture:
+			"https://live.staticflickr.com/665/20380323723_985dc7816f_b.jpg",
 		shopid: shopId,
 	},
 	{
@@ -28,7 +31,8 @@ export const data = [
 		price: 39.99,
 		description:
 			"Easy to set up and store inflatable pool for summer relaxation.",
-		picture: "",
+		picture:
+			"https://s3-eu-west-1.amazonaws.com/images.linnlive.com/38935d6c7534f8ba39b9f613ceef1888/5678b5c2-6a6a-498f-bf50-af04d8a24681.jpg",
 		shopid: shopId,
 	},
 	{
@@ -36,7 +40,8 @@ export const data = [
 		price: 5.99,
 		description:
 			"Pack of 100 colorful water balloons for epic water fights.",
-		picture: "",
+		picture:
+			"https://summeradventure.capitalregionboces.org/wp-content/uploads/2020/07/AdobeStock_351766770-scaled.jpeg",
 		shopid: shopId,
 	},
 	{
@@ -44,42 +49,47 @@ export const data = [
 		price: 12.99,
 		description:
 			"Complete kit with everything you need to build amazing sandcastles.",
-		picture: "",
+		picture:
+			"https://images-na.ssl-images-amazon.com/images/I/712pRRIoIPL._SL1500_.jpg",
 		shopid: shopId,
 	},
 	{
 		name: "Kite",
 		price: 19.99,
 		description: "Easy-to-fly kite for beach or park adventures.",
-		picture: "",
+		picture: "https://weknowyourdreams.com/images/kite/kite-04.jpg",
 		shopid: shopId,
 	},
 	{
 		name: "Bubble wand",
 		price: 4.99,
 		description: "Create endless bubbles with this fun bubble wand.",
-		picture: "",
+		picture:
+			"http://s7.orientaltrading.com/is/image/OrientalTrading/16_1210?$VIEWER_ZOOM$&$NOWA$",
 		shopid: shopId,
 	},
 	{
 		name: "Water gun",
 		price: 14.99,
 		description: "Powerful water gun for epic water battles with friends.",
-		picture: "",
+		picture:
+			"https://i5.walmartimages.com/asr/32dbaa24-4478-4967-b46c-6ca40fff305a_1.10fa283b1f0a0819eb5cbda95ae5a616.jpeg",
 		shopid: shopId,
 	},
 	{
 		name: "Surfboard",
 		price: 299.99,
 		description: "High-quality surfboard for riding the waves this summer.",
-		picture: "",
+		picture:
+			"https://www.surfstitch.com/on/demandware.static/-/Sites-ss-master-catalog/default/dwbcabfc3f/images/CLSSHDRAIN/RAINBOW-BOARDSPORTS-SURF-RSC-SURFBOARDS-SURFBOARDS-CLSSHDRAIN_1.JPG",
 		shopid: shopId,
 	},
 	{
 		name: "Paddleball set",
 		price: 29.99,
 		description: "Two-player paddleball set for beach or park fun.",
-		picture: "",
+		picture:
+			"https://i5.walmartimages.com/asr/62a609b8-6452-4861-bf78-649c44f56c42_1.54ec39fb46cece9a47cadf80f70497b8.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff",
 		shopid: shopId,
 	},
 	{
@@ -87,7 +97,8 @@ export const data = [
 		price: 79.99,
 		description:
 			"Classic roller skates for cruising around town or the boardwalk.",
-		picture: "",
+		picture:
+			"https://www.health.com/thmb/6fNro9as8qi2H1SQMUGORgOaJxo=/1200x0/filters:no_upscale():max_bytes(150000):strip_icc()/Rollerblade-Womens-Zetrablade-Inline-Skates-a175a9a1765a4216a4946882d4637bd7.jpeg",
 		shopid: shopId,
 	},
 	{
@@ -95,7 +106,8 @@ export const data = [
 		price: 6.99,
 		description:
 			"Set of colorful pool noodles for endless summer fun in the water.",
-		picture: "",
+		picture:
+			"https://toptenproductreview.com/wp-content/uploads/2019/07/9-5.jpg",
 		shopid: shopId,
 	},
 	{
@@ -103,7 +115,8 @@ export const data = [
 		price: 39.99,
 		description:
 			"Giant-sized Jenga game for outdoor summer fun with friends and family.",
-		picture: "",
+		picture:
+			"https://www.leisurehire.com/wp-content/uploads/2017/02/20180613_183952-1.jpg",
 		shopid: shopId,
 	},
 	{
@@ -111,7 +124,8 @@ export const data = [
 		price: 29.99,
 		description:
 			"Large and colorful flamingo pool float for relaxing summer days.",
-		picture: "",
+		picture:
+			"https://cdn.shopify.com/s/files/1/0209/1522/products/inflatables-giant-inflatable-pink-flamingo-1_1024x1024.jpg?v=1498771633",
 		shopid: shopId,
 	},
 	{
@@ -119,7 +133,8 @@ export const data = [
 		price: 9.99,
 		description:
 			"Fun and easy to use squirt gun for water fights and outdoor play.",
-		picture: "",
+		picture:
+			"http://s7.orientaltrading.com/is/image/OrientalTrading/12_9084?$VIEWER_ZOOM$&$NOWA$",
 		shopid: shopId,
 	},
 	{
@@ -127,15 +142,17 @@ export const data = [
 		price: 19.99,
 		description:
 			"Official size and weight volleyball for beach and outdoor play.",
-		picture: "",
+		picture:
+			"https://images-na.ssl-images-amazon.com/images/I/71K%2Bc77dhvL.jpg",
 		shopid: shopId,
 	},
 	{
-		name: "Kite",
-		price: 14.99,
+		name: "Portable Hammock",
+		price: 79.99,
 		description:
-			"Colorful kite for fun and relaxing days at the park or beach.",
-		picture: "",
+			"Enjoy a relaxing day at the beach or park with this lightweight and easy-to-set-up portable hammock. Made from durable and breathable materials, it's the perfect way to unwind and take in the great outdoors.",
+		picture:
+			"https://ae01.alicdn.com/kf/HTB1hlxnfJzJ8KJjSspkq6zF7VXaf/HLC-Portable-Hammock-for-Camping-Backpacking-Hiking-Woven-Cotton-Fabric-Rainbow-Striped-With-a-Carry-Bag.jpg",
 		shopid: shopId,
 	},
 	{
@@ -143,15 +160,17 @@ export const data = [
 		price: 39.99,
 		description:
 			"Large and sturdy beach umbrella for sun protection and relaxation.",
-		picture: "",
+		picture:
+			"https://beachbrella.com/wp-content/uploads/2014/10/Umbrella-Profile-Enhancement.jpg",
 		shopid: shopId,
 	},
 	{
-		name: "Sandcastle building kit",
-		price: 12.99,
+		name: "Beach towel",
+		price: 24.99,
 		description:
-			"Complete sandcastle building kit with molds and tools for endless fun at the beach.",
-		picture: "",
+			"Dry off in style with this colorful and absorbent beach towel. With a soft and comfortable texture, it's perfect for lounging on the sand or drying off after a refreshing swim in the ocean.",
+		picture:
+			"https://www.thespruce.com/thmb/EbO6FNFnQonomzyYOGrH0t0sINM=/5100x3400/filters:fill(auto,1)/striped-beach-towel-on-sand-with-hat-and-glasses-463236755-5a1f20d5b39d030039f78644.jpg",
 		shopid: shopId,
 	},
 ]

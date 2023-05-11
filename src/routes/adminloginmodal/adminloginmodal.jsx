b4f551bbbfe5f594ff48811e516b65a522c99e9d
Forslunddavid <forslund.david@gmail.com>
2023-05-11 17:32:43 +0200

    log in

--- src/routes/adminloginmodal/adminloginmodal.jsx
+++ src/routes/adminloginmodal/adminloginmodal.jsx
@@ -3,14 +3,21 @@ import { useRecoilState } from "recoil"
 import loggedInUserState from "../state/userState"
 import { url, shopId } from "../../data/constants"
 import "./adminloginmodal.css"
-
 const LoginModal = () => {
 	const [username, setUsername] = useState("")
 	const [password, setPassword] = useState("")
 	const [valid, setValid] = useState(false)
 	const [errors, setErrors] = useState({})
 	const [loggedInUser, setLoggedInUser] = useRecoilState(loggedInUserState)
-
+	const [data, setData] = useState()
+	useEffect(() => {
+		if (data) {
+			console.log(data, "data")
+			console.log(loggedInUser, "logged in user1")
+			setLoggedInUser(data.status)
+			console.log(loggedInUser, "logged in user2")
+		}
+	}, [data])
 	const handleLogin = async (e) => {
 		e.preventDefault()
 		const valid = validateForm()
@@ -24,20 +31,12 @@ const LoginModal = () => {
 				}
 			)
 			console.log(response.status, "response status")
-			const data = await response.json()
+			setData(await response.json())
 			console.log(data, "data")
-			useEffect(() => {
-				if (data.success) {
-					console.log(loggedInUser, "logged in user1")
-					setLoggedInUser(data.user)
-					console.log(loggedInUser, "logged in user2")
-				}
-			}, [data.success])
 		} else {
 			setErrors({ general: "Invalid username/password" })
 		}
 	}
-
 	const validateForm = () => {
 		let valid = true
 		const errors = {}
@@ -52,14 +51,17 @@ const LoginModal = () => {
 		setValid(valid)
 		return valid
 	}
-
 	return (
 		<>
-			<section className="overlay">
+			<section className="login-section">
+				{" "}
 				<section className="modal">
-					<h1>Log In</h1>
+					{" "}
+					<h1>Log In</h1>{" "}
 					<form onSubmit={handleLogin}>
+						{" "}
 						<div className="login-input-container">
+							{" "}
 							<legend className="login-guide-text">
 								Username
 							</legend>
@@ -68,19 +70,20 @@ const LoginModal = () => {
 								placeholder="Name"
 								onChange={(e) => setUsername(e.target.value)}
 							/>
-						</div>
+						</div>{" "}
 						<div className="login-input-container">
+							{" "}
 							<legend className="login-guide-text">
 								Password
 							</legend>
 							<input
 								type="password"
-								placeholder="Minimum 5 tecken"
+								placeholder="Password"
 								onChange={(e) => setPassword(e.target.value)}
 							/>
 							<span className="display-error">
 								{errors.username}
-							</span>
+							</span>{" "}
 						</div>
 						<button
 							className="login-button"
@@ -91,12 +94,11 @@ const LoginModal = () => {
 							}}
 						>
 							Logga in
-						</button>
-					</form>
-				</section>
+						</button>{" "}
+					</form>{" "}
+				</section>{" "}
 			</section>
 		</>
 	)
 }
-
 export default LoginModal

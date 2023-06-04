b6719d7d8fce5e14eaf80a6e179f856bf60e90c9
Forslunddavid <forslund.david@gmail.com>
2023-06-04 17:48:48 +0200

    bg image test

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom"
 import Header from "./header/Header"
 import Footer from "./footer/Footer"
-import bgImage from "../assets/backgroundimage.svg"
+// import bgImage from "../assets/backgroundimage.svg"
 
 const Root = () => (
 	<>
 		<Header />
 		<main>
-			<div
+			{/* <div
 				style={{
 					backgroundImage: `url(${bgImage})`,
 					backgroundSize: "cover",
 				}}
-			></div>
+			></div> */}
 			<Outlet />
 		</main>
 		<Footer />
